c8a407841e7e4410672a2b6f56f2289d54bd33b6
Edson Luiz <edsonsilvajr21@gmail.com>
2021-03-19 16:52:39 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  final Map json = {
+    'nome': 'Edson',
+    'objetos': [
+      {'nome': 'Edson do objeto'},
+      {'nome2': 'Edson do objeto'},
+    ]
+  };
+  print(json);
+  //runApp(MyApp());
+}
